5a41a004c786f25c63a5107048b614e8eca74ac1
Thanushh <thanush2112@gmail.com>
2022-12-05 14:57:50 +0530

    changed url

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-const url = 'http://localhost:5000/posts'
+//http://localhost:5000/posts
+
+const url = 'https://memories-app-tb.cyclic.app/posts'
 
 export const fetchPosts = () => axios.get(url)
 export const createPost = (newPost) => axios.post(url, newPost);
