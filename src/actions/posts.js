b1bbae81e07538355104199a37327893c2d3cc64
Thanushh <thanush2112@gmail.com>
2022-12-05 13:41:05 +0530

    first commit

---
+++ src/actions/posts.js
@@ -0,0 +1,54 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
+import * as api from '../api'
+
+// Action Creators
+export const getPosts = () => async(dispatch) => {  //thunk syntax to access the dispatch....
+    try {
+        const { data } = await api.fetchPosts();
+
+        dispatch({type: FETCH_ALL, payload: data}) ;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export const createPost = (post) => async (dispatch) => {
+    try {
+        const { data} = await api.createPost(post)
+
+        dispatch({ type: CREATE, payload: data })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const updatePost = (id, post ) => async (dispatch) => {
+    try {
+        const { data } = await api.updatePost(id, post);
+
+        dispatch({ type: UPDATE, payload: data })
+        console.log("WOrking till posts action")
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const deletePost = (id) => async (dispatch) => {
+    try {
+        await api.deletePost(id)
+
+        dispatch({ type: DELETE, payload: id })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const likePost = (id) => async (dispatch) => {
+    try {
+        const { data } = await api.likePost(id);
+
+        dispatch({ type: LIKE, payload: data })
+    } catch (error) {
+        console.log(error);
+    }
+}
